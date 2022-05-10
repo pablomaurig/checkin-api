c062cc5714a47a3e539a5207d60073b773894aa3
Ivan Leandro Parola <71568112+ivanparola@users.noreply.github.com>
2022-05-10 19:51:00 -0300

    Se agrego metodo get all, patch y delete para admin y empleado (#8)

--- src/services/booking.service.ts
+++ src/services/booking.service.ts
@@ -1,7 +1,20 @@
+import boom from '@hapi/boom';
+import { QueryDeepPartialEntity } from 'typeorm/query-builder/QueryPartialEntity';
 import { Booking } from '../entities/booking.entity';
-import { CreateBooking, BookingStatus } from '../types/booking.types';
+import {
+  Booking as BookingInterface,
+  CreateBooking,
+  BookingStatus,
+} from '../types/booking.types';
 
 class BookingService {
+  async getBookings() {
+    const bookings = await Booking.find();
+    return bookings.filter(booking => {
+      return booking.enable;
+    });
+  }
+
   async createBooking(body: CreateBooking) {
     const { bookingNumber, surname, startDate, endDate, amountGuests } = body;
 
@@ -18,5 +31,41 @@ class BookingService {
 
     return booking.id;
   }
+
+  async updateBooking(id: number, body: Partial<BookingInterface>) {
+    const booking = await Booking.findOneBy({ id: id });
+    if (!booking) {
+      throw boom.notFound('Booking does not exists');
+    }
+
+    const updateBooking = { ...body, updatedAt: new Date() };
+
+    await Booking.update(
+      { id: id },
+      updateBooking as QueryDeepPartialEntity<Booking>
+    );
+
+    const updatedBooking = await Booking.findOneBy({ id: id });
+
+    return updatedBooking;
+  }
+
+  async deleteBooking(id: number) {
+    const booking = await Booking.findOneBy({ id: id });
+    if (!booking) {
+      throw boom.notFound('Booking does not exists');
+    }
+
+    const updateBooking = { updatedAt: new Date(), enable: false };
+
+    await Booking.update(
+      { id: id },
+      updateBooking as QueryDeepPartialEntity<Booking>
+    );
+
+    const updatedBooking = await Booking.findOneBy({ id: id });
+    return updatedBooking;
+  }
 }
+
 export default BookingService;
