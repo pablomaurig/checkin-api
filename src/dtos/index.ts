73bcf3a4ccb776f64242aa70386225d1d7d0a5bb
juandebruijn <juandebruijn@gmail.com>
2022-06-11 16:52:08 -0300

    Agrego servicio Fiware para poder enviar data a orion y updetear data… (#19)
    * Agrego servicio Fiware para poder enviar data a orion y updetear data, ademas se agregan los dtos de habitacion y se envia data desde habitacion al servicio fiware para crear habitacion, el patch aun tiene errores
    
    * Se agrega el envio de datos a Orion pero se deja comentado por fallo en el patch
    
    * Comment imports
    
    * Fix linter

---
+++ src/dtos/index.ts
@@ -0,0 +1,62 @@
+import { Room } from '@entities/room.entity';
+// import { Room as RoomInterface } from '../types/room.types';
+
+export const mapRoomDtoOrion = (room: Room) => {
+  return {
+    id: room.id.toString(),
+    type: 'room',
+    floor: {
+      type: 'Integer',
+      value: room.floor,
+    },
+    name: {
+      type: 'String',
+      value: room.name,
+    },
+    description: {
+      type: 'String',
+      value: room.description,
+    },
+    singleBeds: {
+      type: 'Integer',
+      value: room.singleBeds,
+    },
+    doubleBeds: {
+      type: 'Integer',
+      value: room.doubleBeds,
+    },
+    enable: {
+      type: 'Boolean',
+      value: room.enable,
+    },
+  };
+};
+
+export const mapPropertiesRoomDtoOrion = (room: Room) => {
+  return {
+    floor: {
+      type: 'Integer',
+      value: room.floor,
+    },
+    name: {
+      type: 'String',
+      value: room.name,
+    },
+    description: {
+      type: 'String',
+      value: room.description,
+    },
+    singleBeds: {
+      type: 'Integer',
+      value: room.singleBeds,
+    },
+    doubleBeds: {
+      type: 'Integer',
+      value: room.doubleBeds,
+    },
+    enable: {
+      type: 'Boolean',
+      value: room.enable,
+    },
+  };
+};
